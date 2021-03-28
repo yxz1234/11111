a929c195d7a2bf3f3f672b538857c27da598b264
yxz1234 <80023043+yxz1234@users.noreply.github.com>
2021-03-28 17:36:51 +0800

    Update PGNImage.java

--- 跨平台图像浏览系统解决方案/PGNImage.java
+++ 跨平台图像浏览系统解决方案/PGNImage.java
@@ -1,10 +1,10 @@
-package ¿çÆ½Ì¨Í¼Ïñä¯ÀÀÏµÍ³½â¾ö·½°¸;
+package è·¨å¹³å°å›¾åƒæµè§ˆç³»ç»Ÿè§£å†³æ–¹æ¡ˆ;
 
 class PGNImage extends Image{
 	public void parseFile(String fileName) {
 		Matrix m = new Matrix();
-		imp.doPaint(m);;
-		System.out.println(fileName + ",¸ñÊ½ÎªPGN.");
+		imp.doPaint(m);
+		System.out.println(fileName + ",æ ¼å¼ä¸ºPGN.");
 	}
 
 }
